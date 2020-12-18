d5874041ba67dea6919310fdd3cf5146a861ee67
weaselflink <stefan@stefanbissell.de>
2020-12-18 15:40:05 +0100

    clean code

--- src/main/kotlin/chapter1/CoroutineCount.kt
+++ src/main/kotlin/chapter1/CoroutineCount.kt
@@ -1,19 +1,23 @@
 package chapter1
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     runBlocking {
-        val job = launch {
-            repeat(1_000_000) {
-                launch {
-                    delay(Long.MAX_VALUE)
-                }
-            }
-        }
+        val job = launchSwarm()
         job.cancel()
     }
     println("No problem ...")
 }
+
+fun CoroutineScope.launchSwarm() =
+    launch {
+        repeat(1_000_000) {
+            launch {
+                delay(Long.MAX_VALUE)
+            }
+        }
+    }

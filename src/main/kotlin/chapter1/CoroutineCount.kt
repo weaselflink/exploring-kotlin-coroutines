76c15b53f32eb95a98e6a05c8c69898e008c9694
weaselflink <stefan@stefanbissell.de>
2020-12-18 15:17:54 +0100

    add dispatcher demo

--- src/main/kotlin/chapter1/CoroutineCount.kt
+++ src/main/kotlin/chapter1/CoroutineCount.kt
@@ -1,13 +1,12 @@
 package chapter1
 
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     runBlocking {
-        val job = GlobalScope.launch {
+        val job = launch {
             repeat(1_000_000) {
                 launch {
                     delay(Long.MAX_VALUE)
---
+++ src/main/kotlin/chapter2/WhoExecutesMe.kt
@@ -0,0 +1,44 @@
+package chapter2
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newFixedThreadPoolContext
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
+
+fun main() {
+    runBlocking {
+        runSwarm(newFixedThreadPoolContext(2, "two thread"))
+        runSwarm(newSingleThreadContext("single thread"))
+        runSwarm(Dispatchers.Default)
+        runSwarm(Dispatchers.IO)
+        runSwarm(Dispatchers.Unconfined)
+    }
+}
+
+fun CoroutineScope.runSwarm(context: CoroutineContext) {
+    val swarmSize = 100
+    val channel = Channel<Long>(Channel.UNLIMITED)
+    repeat(swarmSize) {
+        launch(context) {
+            channel.send(Thread.currentThread().id)
+            delay(100)
+        }
+    }
+    launch(context) {
+        val count = flow {
+            repeat(swarmSize) {
+                emit(channel.receive())
+            }
+        }.toList().distinct().count()
+
+        println("${Thread.currentThread().name.substringBefore(" @").substringBefore("-")}: thread count $count")
+    }
+}

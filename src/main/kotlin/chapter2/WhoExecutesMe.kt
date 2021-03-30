779b02b9de5fd713c09382c1bf4b5656a5c2d03a
weaselflink <stefan@stefanbissell.de>
2021-03-30 16:40:25 +0200

    improve existing example

--- src/main/kotlin/chapter2/WhoExecutesMe.kt
+++ src/main/kotlin/chapter2/WhoExecutesMe.kt
@@ -1,6 +1,5 @@
 package chapter2
 
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.channels.Channel
@@ -15,35 +14,38 @@ import kotlin.coroutines.CoroutineContext
 
 @OptIn(ObsoleteCoroutinesApi::class)
 fun main() {
-    runBlocking {
-        runSwarm(newFixedThreadPoolContext(2, "two thread"))
-        runSwarm(newSingleThreadContext("single thread"))
-        runSwarm(Dispatchers.Default)
-        runSwarm(Dispatchers.IO)
-        runSwarm(Dispatchers.Unconfined)
-    }
+    runSwarm(newFixedThreadPoolContext(2, "two thread"))
+    runSwarm(newSingleThreadContext("single thread"))
+    runSwarm(Dispatchers.Default)
+    runSwarm(Dispatchers.IO)
+    runSwarm(Dispatchers.Unconfined)
 }
 
-fun CoroutineScope.runSwarm(context: CoroutineContext) {
-    val swarmSize = 100
-    val channel = Channel<Long>(Channel.UNLIMITED)
-    repeat(swarmSize) {
+fun runSwarm(context: CoroutineContext) {
+    println(">>> $context")
+    runBlocking {
+        val swarmSize = 1000
+        val channel = Channel<Long>(Channel.UNLIMITED)
+        repeat(swarmSize) {
+            launch(context) {
+                channel.send(Thread.currentThread().id)
+                delay(100)
+            }
+        }
         launch(context) {
-            channel.send(Thread.currentThread().id)
-            delay(100)
+            val count = flow {
+                repeat(swarmSize) {
+                    emit(channel.receive())
+                }
+            }.toList().distinct().count()
+
+            println("$dispatcherName: thread count $count")
         }
     }
-    launch(context) {
-        val count = flow {
-            repeat(swarmSize) {
-                emit(channel.receive())
-            }
-        }.toList().distinct().count()
-
-        val dispatcherName = Thread.currentThread()
-            .name
-            .substringBefore(" @")
-            .substringBefore("-")
-        println("$dispatcherName: thread count $count")
-    }
 }
+
+private val dispatcherName
+    get() = Thread.currentThread()
+        .name
+        .substringBefore(" @")
+        .substringBefore("-")

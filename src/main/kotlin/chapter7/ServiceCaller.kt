846e91f4c3b3ebfd82e42b9cdce52a09dc62b627
weaselflink <stefan@stefanbissell.de>
2021-06-07 16:50:09 +0200

    cleanup

--- src/main/kotlin/chapter7/ServiceCaller.kt
+++ src/main/kotlin/chapter7/ServiceCaller.kt
@@ -3,7 +3,6 @@ package chapter7
 import kotlinx.coroutines.future.await
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.supervisorScope
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
